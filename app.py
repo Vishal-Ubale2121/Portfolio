6d8a593461ec6d1b3f08070207c0f468df8a8ee1
Vishal Ubale <45795478+Vishal-Ubale2121@users.noreply.github.com>
2023-10-05 15:02:34 +0530

    'Margins'

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    print('Server is Running......')
+@app.route('/')
+def home():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8085, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
